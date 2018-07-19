0963b79862f6f697e651c7a754d3085c56f6ab04
Kristian Fidrych <kristianfidrych@gmail.com>
2018-07-19 16:29:35 -0400

    Styled responsiveness

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -46,32 +46,28 @@ const NavigationAuth = () =>
     </nav>
 
 const NavigationNonAuth = () =>
-  <nav className="navbar navbar-inverse navbar-fixed-top sticky">
-<div className="container-fluid">
-    <div className="navbar-header">
-    <p id="logo" className={window.location.pathname === "/" ? "active" : ""}>
-        <Link className="logo" to="/"><img className="logoImg img-responsive" src="assets/img/logo.png" alt="cook"/></Link>
-    </p>
-    </div>
-
-    <div id="navbar" className="navbar-collapse collapse">
-        <ul className="nav navbar-nav">
-            <li className={window.location.pathname === "/about" ? "active" : ""}>
-                <Link className="aboutLink" to="/About">About</Link>
-            </li>
-          
-            <li className={window.location.pathname === "register" ? "active" : ""}>
-                <Link className="registerLink" to={routes.SIGN_UP}>Register</Link>
-            </li>
-            <li className={window.location.pathname === "login" ? "active" : ""}>
-                <Link className="loginLink" to={routes.SIGN_IN}>Login</Link>
-            </li>
-            <li className={window.location.pathname === "chatbot" ? "active" : ""}>
-                <Link className="ChatbotLink" to="/Chatbot">Chat With Us!</Link>
-            </li>
-        </ul>
-    </div>
-</div>
-</nav>
+    <nav className="navbar navbar-inverse navbar-fixed-top sticky">
+        <div className="container-fluid">
+            <div id="navbar" className="navbar-collapse collapse">
+                <ul className="nav navbar-nav">
+                    <p id="logo" className={window.location.pathname === "/" ? "active" : ""}>
+                        <Link className="logo" to="/"><img className="logoImg img-responsive" src="assets/img/logo.png" alt="cook"/></Link>
+                    </p>
+                    <li className={window.location.pathname === "/about" ? "active" : ""}>
+                        <Link className="about" to="/About">About</Link>
+                    </li>
+                    <li className={window.location.pathname === "register" ? "active" : ""}>
+                        <Link className="register" to={routes.SIGN_UP}>Register</Link>
+                    </li>
+                    <li className={window.location.pathname === "login" ? "active" : ""}>
+                        <Link className="login" to={routes.SIGN_IN}>Login</Link>
+                    </li>
+                    <li className={window.location.pathname === "chatbot" ? "active" : ""}>
+                        <Link className="ChatbotLink" to="/Contact">Contact!</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
 export default Nav;
